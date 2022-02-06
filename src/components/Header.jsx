b03be03152eebec31c674c129a750bfe9aa83d2e
PaulAkemiS <88691711+PaulAkemiS@users.noreply.github.com>
2022-02-06 19:22:43 -0300

    Agregado de navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(){
+    return(
+        <>
+        <header>
+        <div>
+        <img className="logo" src={require ("../Imagenes/AYUMI.png")} alt ="Diamante"/>
+        </div>
+        </header>
+        </>
+    )  
+}
+
+export default Header;
