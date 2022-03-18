9d90f7b2b6c8304fe94ebf06c9eb65709707684a
PaulAkemiS <88691711+PaulAkemiS@users.noreply.github.com>
2022-03-17 23:05:09 -0300

    FirebaseII

--- src/App.js
+++ src/App.js
@@ -1,37 +1,3 @@
-// import { NavBar } from "./components/NavBar";
-// import "./App.css";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Header from "./components/Header";
-// import ItemListContainer from "./components/ItemListContainer";
-// import ItemDetailContainer from "./components/ItemDetailContainer";
-// import Cart from "./components/Cart";
-// import { CartProvider } from "./components/CartContext";
-
-// function App() {
-//   return (
-//     <>
-//       <CartProvider>
-//         <BrowserRouter>
-//           <Header />
-//           <NavBar />
-
-//           <Routes>
-//             <Route path="/" element={<ItemListContainer />} />
-//             <Route
-//               path="productos/categoria/:categoria"
-//               element={<ItemListContainer />}
-//             />
-//             <Route path="/productos/:id" element={<ItemDetailContainer />} />
-//             <Route path="/carrito" element={<Cart />} />
-//           </Routes>
-//         </BrowserRouter>
-//       </CartProvider>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
